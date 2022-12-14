75b4bd92404a9c5bb58249ef3d89a8fdcca58e40
kalyansagi <kalyansagi.555@gmail.com>
2022-12-14 14:47:00 -0500

    working version

--- src/ReArrangingWords.java
+++ src/ReArrangingWords.java
@@ -5,17 +5,17 @@ first letter with the last one, second letter with the second-to-last one, etc. 
 then the letters should be arranged into C1CnC2Cn-1....
 If s contains an odd number of letters, the middle letter should not be paired with any other letters, but placed at the end of the output string instead.
 Example
-For s = "abedef", the output should be
+For s = "abcdef", the output should be
 solution (s) = "afbecd"
 For s = "abcde", the output should be
-solution (s) = "aebde"
+solution (s) = "aebdc"
 Since s has an odd number of letters, the middle letter 'c' is placed at the end.
 */
 public class ReArrangingWords {
 
     public static void rearrangingWords() {
         StringBuilder str = new StringBuilder();
-        String a = "abcde";
+        String a = "abedef";
 
         if (a.length() % 2 == 0) {
             for (int i = 0; i < a.length() / 2; i++) {
@@ -24,12 +24,13 @@ public class ReArrangingWords {
             }
         } else {
             for (int i = 0; i < a.length() / 2 + 1; i++) {
-                if (a.charAt(i) == a.charAt(a.length() - 1 - i)) {
+                char c = a.charAt(a.length() - 1 - i);
+                if (a.charAt(i) == c) {
                     str = str.append(a.charAt(i));
                     break;
                 }
                 str = str.append(a.charAt(i));
-                str = str.append(a.charAt(a.length() - 1 - i));
+                str = str.append(c);
             }
         }
         System.out.println(str);
